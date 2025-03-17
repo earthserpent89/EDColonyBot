4f2d14c91a51974cccb45b88766830c055a8afe6
zperk13 <46951331+zperk13@users.noreply.github.com>
2025-03-17 00:28:49 -0500

    system_add and Mutex around FileSync

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,52 @@ struct Data {
 }
 
 type Context<'a> = poise::Context<'a, Data, Error>;
+#[poise::command(slash_command)]
+async fn system_add(ctx: Context<'_>, new_system_name: String) -> Result<()> {
+    let gid = match ctx.guild_id() {
+        Some(gid) => gid,
+        None => {
+            ctx.say("This is not a guild/server").await?;
+            return Ok(());
+        }
+    };
+    let reply = ctx.say("Processing...").await?;
+    let mut message = String::from("");
+    ctx.data()
+        .servers
+        .lock()
+        .await
+        .modify(|servers| match servers.get_mut(&gid) {
+            Some(server) => {
+                for system in server.iter() {
+                    if system.name.to_lowercase() == new_system_name {
+                        message = "That system is already registered in this server".into();
+                        return;
+                    }
+                }
+                message = format!("System {new_system_name} registered");
+                server.push(System {
+                    name: new_system_name,
+                    sites: Vec::new(),
+                });
+            }
+            None => {
+                message = format!("System {new_system_name} registered");
+                servers.insert(
+                    gid,
+                    vec![System {
+                        name: new_system_name,
+                        sites: Vec::new(),
+                    }],
+                );
+            }
+        })
+        .unwrap();
+    reply
+        .edit(ctx, CreateReply::default().content(message))
+        .await?;
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -51,12 +97,19 @@ async fn main() -> Result<()> {
     let intents = serenity::GatewayIntents::empty();
 
     let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions { commands: vec![echo()], ..Default::default()})
+        .options(poise::FrameworkOptions {
+            commands: vec![system_add()],
+            ..Default::default()
+        })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {
-                    servers: FileSync::load_or_new(file_sync::Path::new("./servers.json"), HashMap::new(), true)?
+                    servers: Mutex::new(FileSync::load_or_new(
+                        file_sync::Path::new("./servers.json"),
+                        HashMap::new(),
+                        true,
+                    )?),
                 })
             })
         })
